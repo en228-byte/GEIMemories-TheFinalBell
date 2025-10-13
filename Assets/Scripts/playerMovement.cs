d312424e2b280f9fe8646bb5c3673aa1704b2a2c
en228-byte <en228@njit.edu>
2025-10-12 23:06:00 -0400

    created player movement script using placeholder sprites

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public GameObject playerChar;
+
+    public float deltaX = 0.1f;
+    public float deltaY = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //move forward (left to right)
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 movement = new Vector3(deltaX/10, 0, 0);
+            playerChar.transform.Translate ( movement);
+        }
+        //move backwards (right to left)
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 movement = new Vector3(-deltaX/10, 0, 0);
+            playerChar.transform.Translate(movement);
+        }
+        //move up (down to up)
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 movement = new Vector3(0, deltaY/10, 0);
+            playerChar.transform.Translate(movement);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 movement = new Vector3(0, -deltaY/10, 0);
+            playerChar.transform.Translate(movement);
+        }
+
+    }
+}
