eb8b15bb05fa4befa1d9f201d4373aa53a72ae40
if0390 <158608535+if0390@users.noreply.github.com>
2025-10-26 15:16:39 -0400

    Testing
    Testing to see if this will go through for minigames2 and other minigames too.

--- Assets/ObjectFallController.cs
+++ Assets/ObjectFallController.cs
@@ -6,14 +6,10 @@ public class ObjectFallController : MonoBehaviour
 {
     float wait = 0.1f;
     public GameObject fallingObject;
-
-    // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Fall", wait, wait);
     }
-
-    // Update is called once per frame
     void Update()
     {
         Instantiate(fallingObject, new Vector3(Random.Range(-10, 10), 10, 0), Quaternion.identity);
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,13 +8,11 @@ public class PlayerController : MonoBehaviour
     float moveSpeed = 10f;
 
     Rigidbody2D rb;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
