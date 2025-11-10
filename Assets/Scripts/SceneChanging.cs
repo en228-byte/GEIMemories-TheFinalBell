e43d1a8399c0111dafa33d069314a1343de2acbe
en228-byte <en228@njit.edu>
2025-11-09 22:16:24 -0500

    setup for gate integration

--- Assets/Scripts/SceneChanging.cs
+++ Assets/Scripts/SceneChanging.cs
@@ -12,7 +12,8 @@ public class SceneChanging : MonoBehaviour
     }
     public void ChangeScene(string sceneName)
     {
-        Scene scene = SceneManager.GetActiveScene();
+        //Scene scene = SceneManager.GetActiveScene();
+        /*
         if (sceneName.Contains("Memory"))
         {
             SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
@@ -23,6 +24,38 @@ public class SceneChanging : MonoBehaviour
         {
             SceneManager.LoadScene(sceneName);
             SceneManager.UnloadSceneAsync(lastScene);
+        }
+        */
+
+        if (sceneName.Contains("Memory") || sceneName.Contains("minigame"))
+        {
+            //hide gameplay scene objects
+            object[] items = GameObject.FindObjectsOfType(typeof(GameObject));
+            foreach (object item in items)
+            {
+                GameObject current = (GameObject) item;
+                current.SetActive(false);
+            }
+
+            //load next scene
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+        if (sceneName.Contains("gamePlay"))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            if (scene.name == "StartScene")
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+            else
+            {
+                object[] items = GameObject.FindObjectsOfType(typeof(GameObject));
+                foreach (object item in items)
+                {
+                    GameObject current = (GameObject)item;
+                    current.SetActive(true);
+                }
+            }
         }
     }
 }
